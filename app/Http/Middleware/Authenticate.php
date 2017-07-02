a142c87694b39e1de95c7d3a6b9bea77fb0a38a8
Kazuki_Kamizuru <k.amago0721@gmail.com>
2017-07-02 09:57:00 +0900

    fixes #280 認証・認可方式修正（ヘッダで判定）

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -24,12 +24,27 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         // トークンが取得できない
-        if (!($token = $request->get('access_token')) || !($data = Token::getData($token))) {
+        if (!($token = $this->getToken($request)) || !($data = Token::getData($token))) {
             abort(Response::HTTP_UNAUTHORIZED, 'Access token is invalid');
         }
 
         // 取得したデータをリクエストに付与
         $request->merge($data);
         return $next($request);
+    }
+
+    /**
+     * リクエストからアクセストークンを取得します。
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    private function getToken($request)
+    {
+        $authorization = $request->headers->get('Authorization');
+        if (($pos = strpos($authorization, 'Bearer ')) === false) {
+            return '';
+        }
+        return substr($authorization, strlen('Bearer '));
     }
 }
